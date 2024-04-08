e9d6c8cff90a33bf58541b815b127c561049a309
sanyasappadu <sanyasappadu4@gmail.com>
2024-04-08 23:13:16 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/home/:userType" element={<UserHome />} />
           <Route path="/login" element={<Login />} />
         </Routes>
