87089263ec4623aea7fe80eaa0ee22d938ca0db7
sanyasappadu <sanyasappadu4@gmail.com>
2024-05-02 15:26:57 +0530

    flow

--- src/pages/UserHome.jsx
+++ src/pages/UserHome.jsx
@@ -2,7 +2,6 @@ import { useParams } from 'react-router-dom';
 import './home.css'
 const UserHome = () => {
   const { userType } = useParams();
-
   // Function to render features based on user type
   const feature = () => {
     return(
@@ -14,22 +13,66 @@ const UserHome = () => {
         <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Marks</h2></div>
         <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Give Attendance</h2></div>
         <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Logout Account</h2></div>
-
       </div>
     )
   }
   const renderFeatures = () => {
     switch (userType) {
       case 'type1':
-        return <div>Features A-F</div>;
+        return       <div  className='home-side-container'>
+        <div><img className='side-container-img' src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQRt_0WRr8Mc016RGaTK8eaiv6dSHKuNjIwdUrnF_7Xa_GdQL9YX9f4le5qucuyVUpKxbo7gqIGC0pZo14" alt="" /></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>TYPE 1</h2></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>Profile Details</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Create Account</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Accounts</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Marks</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Give Attendance</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Logout Account</h2></div>
+      </div>;
       case 'type2':
-        return <div>Features A-E</div>;
+        return       <div  className='home-side-container'>
+        <div><img className='side-container-img' src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQRt_0WRr8Mc016RGaTK8eaiv6dSHKuNjIwdUrnF_7Xa_GdQL9YX9f4le5qucuyVUpKxbo7gqIGC0pZo14" alt="" /></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>TYPE 2</h2></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>Profile Details</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Create Account</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Accounts</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Marks</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Give Attendance</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Logout Account</h2></div>
+      </div>;
       case 'type3':
-        return <div>Features A-D</div>;
+        return       <div  className='home-side-container'>
+        <div><img className='side-container-img' src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQRt_0WRr8Mc016RGaTK8eaiv6dSHKuNjIwdUrnF_7Xa_GdQL9YX9f4le5qucuyVUpKxbo7gqIGC0pZo14" alt="" /></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>TYPE3</h2></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>Profile Details</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Create Account</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Accounts</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Marks</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Give Attendance</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Logout Account</h2></div>
+      </div>;
       case 'type4':
-        return <div>Features A-C</div>;
+        return       <div  className='home-side-container'>
+        <div><img className='side-container-img' src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQRt_0WRr8Mc016RGaTK8eaiv6dSHKuNjIwdUrnF_7Xa_GdQL9YX9f4le5qucuyVUpKxbo7gqIGC0pZo14" alt="" /></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>TYPE4</h2></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>Profile Details</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Create Account</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Accounts</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Marks</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Give Attendance</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Logout Account</h2></div>
+      </div>;
       case 'type5':
-        return <div>Features A-B</div>;
+        return       <div  className='home-side-container'>
+        <div><img className='side-container-img' src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQRt_0WRr8Mc016RGaTK8eaiv6dSHKuNjIwdUrnF_7Xa_GdQL9YX9f4le5qucuyVUpKxbo7gqIGC0pZo14" alt="" /></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>TYPE5</h2></div>
+        <div className='side-container-box'><h2 style={{marginLeft:"50px"}}>Profile Details</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Create Account</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Accounts</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Manage Marks</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Give Attendance</h2></div>
+        <div  className='side-container-box'><h2 style={{marginLeft:"50px"}}>Logout Account</h2></div>
+      </div>;
       default:
         return <div>Invalid user type</div>;
     }
@@ -38,7 +81,9 @@ const UserHome = () => {
   return (
     <div style={{backgroundColor:"rgb(229, 171, 238)"}}>
       {/* <h2>Welcome to UserHome Page</h2> */}
-      {feature()}
+      {/* {feature()} */}
+      {renderFeatures()}
+      <h1>Sunny</h1>
     </div>
   );
 };
